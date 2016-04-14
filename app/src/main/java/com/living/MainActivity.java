3eba1b8e60436a95779fbfad53725368da353c95
Tobiin <616041023@qq.com>
2016-04-14 15:22:06 +0800

    tobin 4.14

--- app/src/main/java/com/living/MainActivity.java
+++ app/src/main/java/com/living/MainActivity.java
@@ -11,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("h", "hello today");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
