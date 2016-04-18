9615711ecde737943eeedc621e43a780cfbc7b64
minhu <35922234@qq.com>
2016-04-18 16:53:20 +0800

    删了一些没用的东西

--- app/src/main/java/com/living/MainActivity.java
+++ app/src/main/java/com/living/MainActivity.java
@@ -16,13 +16,7 @@ import com.living.activity.NewsActivity;
 import com.living.bean.CountryWeatherBean;
 import com.living.bean.NewsChannelBean;
 import com.living.util.LivingNetUtils;
-import com.living.util.LogUtil;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -86,45 +80,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    String httpUrl = "http://apis.baidu.com/heweather/weather/free";
-    String httpArg = "city=beijing";
-//    String jsonResult = request(httpUrl, httpArg);
-
-    /**
-     * @param httpUrl :请求接口
-     * @param httpArg :参数
-     * @return 返回结果
-     */
-    public static String request(String httpUrl, String httpArg) {
-        BufferedReader reader = null;
-        String result = null;
-        StringBuffer sbf = new StringBuffer();
-        httpUrl = httpUrl + "?" + httpArg;
-
-        try {
-            URL url = new URL(httpUrl);
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            connection.setRequestMethod("GET");
-            // 填入apikey到HTTP header
-            connection.setRequestProperty("apikey", LivingNetUtils.APIKEY);
-            connection.connect();
-            InputStream is = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String strRead = null;
-            while ((strRead = reader.readLine()) != null) {
-                sbf.append(strRead);
-                sbf.append("\r\n");
-            }
-            reader.close();
-            result = sbf.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            LogUtil.e("访问失败");
-        }
-//        LogUtil.e("访问成功:"+result);
-        return result;
-    }
 
     @Override
     public void onStart() {
