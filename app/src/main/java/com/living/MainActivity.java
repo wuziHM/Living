3ca7c5f463524c6eeaae0f5d17214acd2eac1b9a
Tobiin <616041023@qq.com>
2016-05-23 11:43:44 +0800

    tobin 5.18

--- app/src/main/java/com/living/MainActivity.java
+++ app/src/main/java/com/living/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
     }
 
-
     private void initView() {
 
         findViewById(R.id.iv_main_activity_back).setOnClickListener(this);
