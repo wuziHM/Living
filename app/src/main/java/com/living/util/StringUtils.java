c8bfcc008293415bde4af89d927e19a1f3151bee
Tobiin <616041023@qq.com>
2016-05-27 13:29:34 +0800

    tobin 5.26

--- app/src/main/java/com/living/util/StringUtils.java
+++ app/src/main/java/com/living/util/StringUtils.java
@@ -1,6 +1,5 @@
 package com.living.util;
 
-import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
@@ -9,7 +8,7 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringAndDateUtil {
+public class StringUtils {
 
 	private static final long TIME_SECOND = 1 * 1000L;
 	private static final long TIME_MINUTE = 60 * TIME_SECOND;
@@ -114,158 +113,36 @@ public class StringAndDateUtil {
 		return false;
 	}
 
-	public static Date simpleDateParse(String dateString, String format) {
-		SimpleDateFormat sdf = new SimpleDateFormat(format);
-		try {
-			return sdf.parse(dateString);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
+	/**
+	 * 去掉字符串中空格
+	 * @param str
+	 * @return
+     */
 	public static String ignoreStringBlank(String str) {
 		return str.replaceAll("[ 　]", "");
 	}
 
-	public static int ignoreBlankLength(String str) {
-		if (isBlank(str)) {
-			return 0;
-		}
-		return str.replaceAll("[ 　]", "").length();
-	}
-
-	// 判断手机格式是否正确
+	/**
+	 * 判断手机格式是否正确
+	 * @param mobiles
+	 * @return
+     */
 	public static boolean isMobileNumber(String mobiles) {
 		Pattern p = Pattern.compile("^((13[0-9])|(15[0-9])|(18[0-9])|(16[0-9])|(14[0-9])|(17[0-9]))\\d{8}$");
 		Matcher m = p.matcher(mobiles);
 		return m.matches();
 	}
 
+	/**
+	 * 密码格式是否正确
+	 * @param pass
+	 * @return
+     */
 	public static boolean isPassWord(String pass) {
 		// 允许空字符串就最后用*.不允许就+
 		Pattern ptn = Pattern.compile("^[A-Za-z0-9]{6,18}+$");
 		Matcher mth = ptn.matcher(pass);
 		return mth.find();
 	}
-
-	/*********************** 字符串转码 开始 *************************/
-	/**
-	 * 转换字符串的编码
-	 */
-	/** 7位ASCII字符，也叫作ISO646-US、Unicode字符集的基本拉丁块 */
-	public static final String US_ASCII = "US-ASCII";
-
-	/** ISO 拉丁字母表 No.1，也叫作 ISO-LATIN-1 */
-	public static final String ISO_8859_1 = "ISO-8859-1";
-
-	/** 8 位 UCS 转换格式 */
-	public static final String UTF_8 = "UTF-8";
-
-	/** 16 位 UCS 转换格式，Big Endian（最低地址存放高位字节）字节顺序 */
-	public static final String UTF_16BE = "UTF-16BE";
-
-	/** 16 位 UCS 转换格式，Little-endian（最高地址存放低位字节）字节顺序 */
-	public static final String UTF_16LE = "UTF-16LE";
-
-	/** 16 位 UCS 转换格式，字节顺序由可选的字节顺序标记来标识 */
-	public static final String UTF_16 = "UTF-16";
-
-	/** 中文超大字符集 */
-	public static final String GBK = "GBK";
-
-	/**
-	 * 将字符编码转换成US-ASCII码
-	 */
-	public static String toASCII(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, US_ASCII);
-	}
-
-	/**
-	 * 将字符编码转换成ISO-8859-1码
-	 */
-	public static String toISO_8859_1(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, ISO_8859_1);
-	}
-
-	/**
-	 * 将字符编码转换成UTF-8码
-	 */
-	public static String toUTF_8(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, UTF_8);
-	}
-
-	/**
-	 * 将字符编码转换成UTF-16BE码
-	 */
-	public static String toUTF_16BE(String str)
-			throws UnsupportedEncodingException {
-		return changeCharset(str, UTF_16BE);
-	}
-
-	/**
-	 * 将字符编码转换成UTF-16LE码
-	 */
-	public static String toUTF_16LE(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, UTF_16LE);
-	}
-
-	/**
-	 * 将字符编码转换成UTF-16码
-	 */
-	public static String toUTF_16(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, UTF_16);
-	}
-
-	/**
-	 * 将字符编码转换成GBK码
-	 */
-	public static String toGBK(String str) throws UnsupportedEncodingException {
-		return changeCharset(str, GBK);
-	}
-
-	/**
-	 * 字符串编码转换的实现方法
-	 * 
-	 * @param str
-	 *            待转换编码的字符串
-	 * @param newCharset
-	 *            目标编码
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-	public static String changeCharset(String str, String newCharset) throws UnsupportedEncodingException {
-		if (str != null) {
-			// 用默认字符编码解码字符串。
-			byte[] bs = str.getBytes();
-			// 用新的字符编码生成字符串
-			return new String(bs, newCharset);
-		}
-		return null;
-	}
-
-	/**
-	 * 字符串编码转换的实现方法
-	 * 
-	 * @param str
-	 *            待转换编码的字符串
-	 * @param oldCharset
-	 *            原编码
-	 * @param newCharset
-	 *            目标编码
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-	public static String changeCharset(String str, String oldCharset,
-			String newCharset) throws UnsupportedEncodingException {
-		if (str != null) {
-			// 用旧的字符编码解码字符串。解码可能会出现异常。
-			byte[] bs = str.getBytes(oldCharset);
-			// 用新的字符编码生成字符串
-			return new String(bs, newCharset);
-		}
-		return null;
-	}
-	/*********************** 字符串转码 结束 *************************/
 
 }
